1ad4f0558705a7c3e27ed425c16cdc9b9fbf5333
codewitharibamemon <codewitharibamemon@gmail.com>
2025-05-04 20:29:42 +0500

    class_02

---
+++ class_02js/script.js
@@ -0,0 +1,14 @@
+// document.getElementById("ariba").style.color = "red";
+
+// document.getElementById("sameer").style.color = "green";
+
+// document.getElementById("maryam").style.color = "blue";
+
+
+// var a = 2;
+// var b = 5;
+// document.write("Hello World!");
+
+
+// document.cookie
+// document.getElementsByClassName("ariba")
